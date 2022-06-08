0d3fb76d4ecea4490704d261abaa210a86313a85
jvanjas <sanika.vanjari-ext@jci.com>
2022-06-08 18:19:23 +0530

    calculator button actions and type added

--- app/src/main/java/com/example/calculatorcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorcomposeapp/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.calculatorcomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.calculatorcomposeapp.ui.theme.CalculatorComposeAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +17,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                val viewModel = viewModels<CalculatorViewModel>()
+                val state = viewModel.value.state
+                val buttonSpacing = 8.dp
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    CalculatorComposeAppTheme {
-        Greeting("Android")
     }
 }
