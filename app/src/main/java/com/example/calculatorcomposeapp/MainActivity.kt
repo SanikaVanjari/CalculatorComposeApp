0ff63dabaf6d52a78c3e190984c194f5f0caab1f
jvanjas <sanika.vanjari-ext@jci.com>
2022-06-10 16:39:57 +0530

    calculator actions added

--- app/src/main/java/com/example/calculatorcomposeapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorcomposeapp/MainActivity.kt
@@ -4,10 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.calculatorcomposeapp.ui.theme.CalculatorComposeAppTheme
@@ -19,7 +25,16 @@ class MainActivity : ComponentActivity() {
             CalculatorComposeAppTheme {
                 val viewModel = viewModels<CalculatorViewModel>()
                 val state = viewModel.value.state
-                val buttonSpacing = 8.dp
+                val buttonSpacing = 10.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel.value::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.DarkGray)
+                        .padding(16.dp)
+                )
             }
         }
     }
